7d1aa889b6e23ac901404fadf6b2a2d2d1ae585e
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2023-11-15 09:30:27 -0300

    Ejercicio terminado

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -16,32 +16,34 @@ namespace Full_GRASP_And_SOLID
 
         public Product FinalProduct { get; set; }
 
+        public bool cooked { get; private set; } = false;
+
         // Agregado por Creator
-        public void AddStep(Product input, double quantity, Equipment equipment, int time)
+        public void AddStep (Product input, double quantity, Equipment equipment, int time)
         {
-            Step step = new Step(input, quantity, equipment, time);
-            this.steps.Add(step);
+            Step step = new Step (input, quantity, equipment, time);
+            this.steps.Add (step);
         }
 
         // Agregado por OCP y Creator
-        public void AddStep(string description, int time)
+        public void AddStep (string description, int time)
         {
-            WaitStep step = new WaitStep(description, time);
-            this.steps.Add(step);
+            WaitStep step = new WaitStep (description, time);
+            this.steps.Add (step);
         }
 
-        public void RemoveStep(BaseStep step)
+        public void RemoveStep (BaseStep step)
         {
-            this.steps.Remove(step);
+            this.steps.Remove (step);
         }
 
         // Agregado por SRP
-        public string GetTextToPrint()
+        public string GetTextToPrint ()
         {
             string result = $"Receta de {this.FinalProduct.Description}:\n";
             foreach (BaseStep step in this.steps)
             {
-                result = result + step.GetTextToPrint() + "\n";
+                result = result + step.GetTextToPrint () + "\n";
             }
 
             // Agregado por Expert
@@ -51,16 +53,56 @@ namespace Full_GRASP_And_SOLID
         }
 
         // Agregado por Expert
-        public double GetProductionCost()
+        public double GetProductionCost ()
         {
             double result = 0;
 
             foreach (BaseStep step in this.steps)
             {
-                result = result + step.GetStepCost();
+                result = result + step.GetStepCost ();
             }
 
             return result;
+        }
+
+        public int GetCookTime ()
+        {
+            int totalCookTime = 0;
+
+            foreach (BaseStep step in this.steps)
+            {
+                totalCookTime += step.Time;
+            }
+
+            return totalCookTime;
+        }
+
+        public void Cook ()
+        {
+            if (this.cooked == false)
+            {
+                CountdownTimer countdownTimer = new CountdownTimer ();
+                RecipeAdapter recipeAdapter = new RecipeAdapter (this);
+                int cookTime = this.GetCookTime ();
+
+                countdownTimer.Register (cookTime , recipeAdapter);
+            }
+        }
+
+        //Se agrega una "Inner Class" por el patrón 'Adapter'
+        public class RecipeAdapter : TimerClient
+        {
+            public Recipe recipe;
+
+            public RecipeAdapter (Recipe recipe)
+            {
+                this.recipe = recipe;
+            }
+
+            public void TimeOut ()
+            {
+                this.recipe.cooked = true;
+            }
         }
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Full_GRASP_And_SOLID
 {
@@ -16,7 +17,7 @@ namespace Full_GRASP_And_SOLID
 
         private static List<Equipment> equipmentCatalog = new List<Equipment>();
 
-        public static void Main(string[] args)
+        public static void Main (string [] args)
         {
             PopulateCatalogs();
 
@@ -31,6 +32,11 @@ namespace Full_GRASP_And_SOLID
             printer.PrintRecipe(recipe);
             printer = new FilePrinter();
             printer.PrintRecipe(recipe);
+
+            Console.WriteLine($"Cooked: {recipe.cooked}");
+            recipe.Cook();
+            Thread.Sleep(500); // 0.5 segundos
+            Console.WriteLine($"Cooked: {recipe.cooked}");
         }
 
         private static void PopulateCatalogs()
